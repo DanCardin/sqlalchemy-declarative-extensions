5081a71f75b30831dd809b63adc70e807d45ba68
DanCardin <ddcardin@gmail.com>
2024-01-30 11:51:41 -0500

    chore: Include Table in row package init.

--- src/sqlalchemy_declarative_extensions/row/__init__.py
+++ src/sqlalchemy_declarative_extensions/row/__init__.py
@@ -1,8 +1,9 @@
 from sqlalchemy_declarative_extensions.row import compare
-from sqlalchemy_declarative_extensions.row.base import Row, Rows
+from sqlalchemy_declarative_extensions.row.base import Row, Rows, Table
 
 __all__ = [
     "compare",
     "Rows",
     "Row",
+    "Table",
 ]
