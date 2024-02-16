a2c6973724f6d61038dac6d1a6b2515c6193ea3e
DanCardin <ddcardin@gmail.com>
2024-02-16 15:44:19 -0500

    chore: Remove support for 3.7.
    Dependencies of the library are beginning to require 3.8 or 3.9 or
    higher, and it will just become more of a problem. For now, we're okay
    to sunset just 3.8.

--- src/sqlalchemy_declarative_extensions/typing.py
+++ src/sqlalchemy_declarative_extensions/typing.py
@@ -1,10 +1,4 @@
-import sys
-
-if sys.version_info >= (3, 8):
-    from typing import Protocol, runtime_checkable
-else:
-    from typing_extensions import Protocol, runtime_checkable
-
+from typing import Protocol, runtime_checkable
 
 __all__ = [
     "Protocol",
