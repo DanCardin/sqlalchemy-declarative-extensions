914c57e1b1833b539380a500303028d4c6671704
DanCardin <ddcardin@gmail.com>
2024-09-28 11:39:15 -0400

    fix: Handle procedure metadata sequence.

--- src/sqlalchemy_declarative_extensions/procedure/ddl.py
+++ src/sqlalchemy_declarative_extensions/procedure/ddl.py
@@ -10,7 +10,7 @@ from sqlalchemy_declarative_extensions.sql import match_name
 
 def procedure_ddl(procedures: Procedures, procedure_filter: list[str] | None = None):
     def after_create(metadata: MetaData, connection: Connection, **_):
-        result = compare_procedures(connection, procedures, metadata)
+        result = compare_procedures(connection, procedures)
         for op in result:
             if not match_name(op.procedure.qualified_name, procedure_filter):
                 continue
