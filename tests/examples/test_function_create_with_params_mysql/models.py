84b214660db0877ce5848af00cf21df5523f2a5f
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2025-04-02 01:11:10 +0200

    Add alembic tests with MySQL parametric function

---
+++ tests/examples/test_function_create_with_params_mysql/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, types
+from sqlalchemy.orm import declarative_base
+
+from sqlalchemy_declarative_extensions import Functions, declarative_database
+from sqlalchemy_declarative_extensions.dialects.mysql import (
+    Function,
+    FunctionDataAccess,
+)
+
+_Base = declarative_base()
+
+
+@declarative_database
+class Base(_Base): # type: ignore
+    __abstract__ = True
+
+    functions = Functions().are(
+        Function(
+            "add_deterministic",
+            "RETURN i + 1;",
+            parameters=["i integer"],
+            returns="INTEGER",
+            deterministic=True,
+            data_access=FunctionDataAccess.no_sql,
+        )
+    )
+
+
+# Include a dummy table just in case it's needed
+class DummyTable(Base):
+    __tablename__ = "dummy_table"
+    id = Column(types.Integer, primary_key=True)
